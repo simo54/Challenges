cfeb3c2490fc2d53f819338873a2a227c4a79316
Simo <sorzesimone1@gmail.com>
2021-01-04 13:12:51 +0100

    removing unnecessary code

--- PostWordCutter/index.js
+++ PostWordCutter/index.js
@@ -2,25 +2,32 @@
 const post_integral =
   "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
 
-const char_preview = 20;
+const char_preview = 100;
 
 const preview_maker = (post, preview_characters) => {
   // Cutting the string and get raw preview
   let raw_preview = post.slice(0, preview_characters);
-  console.log("Raw Preview: " + raw_preview);
+
   // Getting the last character of the preview
   let last_char = raw_preview.slice(-1);
 
-  // If last char ends with a white space
   if (last_char.endsWith(" ")) {
-    console.log("Formatted Preview: " + raw_preview.trim() + "...");
+    // Remove all white spaces and add dots
+    console.log(
+      "Output where preview ends with white space: " +
+        raw_preview.trim() +
+        "..."
+    );
+    return;
   } else {
-    // Go back until find a white space
+    // Loop until find a white space, remove unnecessary character and white spaces
     for (let i = raw_preview.length - 1; i >= 0; i--) {
-      console.log(raw_preview[i]);
-
       if (raw_preview[i] === " ") {
-        console.log(raw_preview.slice(0, i).trim() + "...");
+        console.log(
+          "Output where preview ends with character: " +
+            raw_preview.slice(0, i).trim() +
+            "..."
+        );
         return;
       }
     }
